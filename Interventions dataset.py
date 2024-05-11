8f1247fce1a52e7d23aa4758093d497e8f6f183c
JeroenGuillierme <162816985+JeroenGuillierme@users.noreply.github.com>
2024-05-11 19:32:12 +0200

    Create Interventions dataset.py
    Response times (T3-T0) interventions data

---
+++ Interventions dataset.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from datetime import datetime
+import numpy as np
+
+ambulance = pd.read_parquet('DATA/ambulance_locations.parquet.gzip')
+mug = pd.read_parquet('DATA/mug_locations.parquet.gzip')
+pit = pd.read_parquet('DATA/pit_locations.parquet.gzip')
+interventions1 = pd.read_parquet('DATA/interventions1.parquet.gzip')
+interventions2 = pd.read_parquet('DATA/interventions2.parquet.gzip')
+interventions3 = pd.read_parquet('DATA/interventions3.parquet.gzip')
+interventions4 = pd.read_parquet('DATA/interventions_bxl.parquet.gzip')
+interventions5 = pd.read_parquet('DATA/interventions_bxl2.parquet.gzip')
+cad = pd.read_parquet('DATA/cad9.parquet.gzip')
+aed = pd.read_parquet('DATA/aed_locations.parquet.gzip')
+
+pd.set_option('display.max_columns', None)
+
+
+interventions1["T0"] = pd.to_datetime(interventions1["T0"], format='%d%b%y:%H:%M:%S') # Convert the first time format to datetime
+interventions1["T3"] = pd.to_datetime(interventions1["T3"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+
+interventions2["T0"] = pd.to_datetime(interventions2["T0"], format='%d%b%y:%H:%M:%S') # Convert the first time format to datetime
+interventions2["T3"] = pd.to_datetime(interventions2["T3"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+
+interventions3["T0"] = pd.to_datetime(interventions3["T0"], format='%d%b%y:%H:%M:%S') # Convert the first time format to datetime
+interventions3["T3"] = pd.to_datetime(interventions3["T3"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+
+interventions_total = pd.concat([interventions1, interventions2, interventions3], axis=0)
+#print(interventions_total)
+
+interventions_total['CAD9'] = 'N'
+interventions_total["T3-T0"] = interventions_total["T3"]-interventions_total["T0"]
+interventions_total = interventions_total[interventions_total["EventType Firstcall"] == "P003 - Cardiac arrest"]
+print(interventions_total["EventLevel Firstcall"])
+#print(interventions_total["T3-T0"])
+interventions_total = interventions_total["Lattitude intervention", "Longitude intervention", "CAD9", "EventLevel Firstcall", "T0-T3"]
+
+CAD9_expanded = cad
+#print(CAD9_expanded.head())
+CAD9_expanded['CAD9'] = 'Y'
+
+CAD9_expanded["T0"] = pd.to_datetime(CAD9_expanded["T0"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+CAD9_expanded["T3"] = pd.to_datetime(CAD9_expanded["T3"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+
+CAD9_expanded["T3-T0"] = CAD9_expanded["T3"]-CAD9_expanded["T0"]
+
+CAD9_expanded.loc[CAD9_expanded['T3-T0'] < pd.Timedelta(0), 'T3-T0'] = pd.NaT
+CAD9_expanded = CAD9_expanded[CAD9_expanded["EventType Trip"] == "P003 - HARTSTILSTAND - DOOD - OVERLEDEN"]
+print(len(CAD9_expanded))
+
+#print(CAD9_expanded["T3-T0"])
+#interventions_total = interventions_total[""]
+
+#interventions_total2 = pd.concat([interventions_total, CAD9_expanded], axis=0)
+#print(interventions_total2)
+#interventions_total2 = interventions_total2["Lattitude intervention", "Longitude intervention", "CAD9", "EventLevel Firstcall", "T0-T3"]
