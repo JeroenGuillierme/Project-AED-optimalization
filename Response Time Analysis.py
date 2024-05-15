d8d7207b39d670cdaf633689ecfcc141df8e4c20
ElenaPollet <168332247+ElenaPollet@users.noreply.github.com>
2024-05-15 17:18:42 +0200

    Update Response Time Analysis.py

--- Response Time Analysis.py
+++ Response Time Analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 ambulance = pd.read_parquet('DATA/ambulance_locations.parquet.gzip')
 mug = pd.read_parquet('DATA/mug_locations.parquet.gzip')
@@ -11,57 +12,89 @@ interventions5 = pd.read_parquet('DATA/interventions_bxl2.parquet.gzip')
 cad = pd.read_parquet('DATA/cad9.parquet.gzip')
 aed = pd.read_parquet('DATA/aed_locations.parquet.gzip')
 
+pd.set_option('display.max_columns', None)
 
 
-## ambulance, mug, pit en aed bevatten geen responstijden dus gebruiken we niet
+##### DATA PRE-PREPROCESSING #####
+""" Ambulance, mug, pit en aed bevatten geen responstijd variabelen dus gebruiken we niet """
 
 
 # interventions1: CityName permanence, CityName intervention, Vector type, EventLevel Firstcall
+interventions1["T0"] = pd.to_datetime(interventions1["T0"], format='%d%b%y:%H:%M:%S') # Convert the first time format to datetime
+interventions1["T3"] = pd.to_datetime(interventions1["T3"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+interventions1["T5"] = pd.to_datetime(interventions1["T5"], format='%Y-%m-%d %H:%M:%S.%f')
+
 df1 = pd.DataFrame({
     "CityName permanence": interventions1["CityName permanence"].str.upper(),
     "CityName intervention": interventions1["CityName intervention"].str.upper(),
     "Vector type": interventions1["Vector type"].str.upper(),
-    "EventLevel Firstcall": interventions1["EventLevel Firstcall"].str.upper()})
+    "EventLevel Firstcall": interventions1["EventLevel Firstcall"].str.upper(),
+    "Time1": interventions1["T3"]-interventions1["T0"],
+    "Time2": interventions1["T5"]-interventions1["T0"]})
 df1["CityName permanence"] = df1["CityName permanence"].str.extract(r'\((.*?)\)')
 df1["CityName intervention"] = df1["CityName intervention"].str.extract(r'\((.*?)\)')
-#print(df1["EventLevel Firstcall"])
+#print(df1["Time2"])
 
 # interventions2: CityName permanence, CityName intervention, Vector type, EventLevel Firstcall
+interventions2["T0"] = pd.to_datetime(interventions2["T0"], format='%d%b%y:%H:%M:%S') # Convert the first time format to datetime
+interventions2["T3"] = pd.to_datetime(interventions2["T3"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+interventions2["T5"] = pd.to_datetime(interventions2["T5"], format='%Y-%m-%d %H:%M:%S.%f')
+
 df2 = pd.DataFrame({"CityName permanence": interventions2["CityName permanence"].str.upper(),
                     "CityName intervention": interventions2["CityName intervention"].str.upper(),
                     "Vector type": interventions2["Vector type"].str.upper(),
-                    "EventLevel Firstcall": interventions2["EventLevel Firstcall"].str.upper()})
+                    "EventLevel Firstcall": interventions2["EventLevel Firstcall"].str.upper(),
+                    "Time1": interventions2["T3"]-interventions2["T0"],
+                    "Time2": interventions2["T5"]-interventions2["T0"]})
 df2["CityName permanence"] = df2["CityName permanence"].str.extract(r'\((.*?)\)')
 df2["CityName intervention"] = df2["CityName intervention"].str.extract(r'\((.*?)\)')
-#print(df2["EventLevel Firstcall"])
+#print(df2["Time2"])
 
 
 # interventions3: CityName permanence, CityName intervention, Vector type, EventLevel Firstcall
+interventions3["T0"] = pd.to_datetime(interventions3["T0"], format='%d%b%y:%H:%M:%S') # Convert the first time format to datetime
+interventions3["T3"] = pd.to_datetime(interventions3["T3"], format='%Y-%m-%d %H:%M:%S.%f')# Convert the column back to datetime with the new format
+interventions3["T5"] = pd.to_datetime(interventions3["T5"], format='%Y-%m-%d %H:%M:%S.%f')
+
 df3 = pd.DataFrame({"CityName permanence": interventions3["CityName permanence"].str.upper(),
                     "CityName intervention": interventions3["CityName intervention"].str.upper(),
                     "Vector type": interventions3["Vector type"].str.upper(),
-                    "EventLevel Firstcall": interventions3["EventLevel Firstcall"].str.upper()})
+                    "EventLevel Firstcall": interventions3["EventLevel Firstcall"].str.upper(),
+                    "Time1": interventions3["T3"]-interventions3["T0"],
+                    "Time2": interventions3["T5"]-interventions3["T0"]})
 df3["CityName permanence"] = df3["CityName permanence"].str.extract(r'\((.*?)\)')
 df3["CityName intervention"] = df3["CityName intervention"].str.extract(r'\((.*?)\)')
-#print(df3["EventLevel Firstcall"])
+#print(df3["Time2"])
 
 # interventions4: cityname_permanence, CityName intervention, vector_type, eventLevel_firstcall
+interventions4["t0"] = pd.to_datetime(interventions4["t0"], format='%Y-%m-%d %H:%M:%S.%f %z')# Convert the first time format to datetime
+interventions4["t3"] = pd.to_datetime(interventions4["t3"], format='%Y-%m-%d %H:%M:%S.%f %z') # Convert the column back to datetime with the new format
+interventions4["t5"] = pd.to_datetime(interventions4["t5"], format='%Y-%m-%d %H:%M:%S.%f %z')
+
 df4 = pd.DataFrame({"CityName permanence": interventions4["cityname_permanence"].str.upper(),
                     "CityName intervention": interventions4["cityname_intervention"].str.upper(),
                     "Vector type": interventions4["vector_type"].str.upper(),
-                    "EventLevel Firstcall": interventions4["eventLevel_firstcall"].str.upper()})
+                    "EventLevel Firstcall": interventions4["eventLevel_firstcall"].str.upper(),
+                    "Time1": interventions4["t3"]-interventions4["t0"],
+                    "Time2": interventions4["t5"]-interventions4["t0"]})
 df4["CityName permanence"] = df4["CityName permanence"].str.split(" \(").str[0]
 df4["CityName permanence"] = df4["CityName permanence"].replace("BRUXELLES", "BRUSSEL")
 df4["CityName intervention"] = df4["CityName intervention"].str.split(" \(").str[0]
 df4["CityName intervention"] = df4["CityName intervention"].replace("BRUXELLES", "BRUSSEL")
-#print(df4["EventLevel Firstcall"])
+#print(df4["Time2"])
 
 
 #interventions5
+interventions5["T0"] = pd.to_datetime(interventions5["T0"], format='%d%b%y:%H:%M:%S')# Convert the first time format to datetime
+interventions5["T3"] = pd.to_datetime(interventions5["T3"], format='%d%b%y:%H:%M:%S') # Convert the column back to datetime with the new format
+interventions5["T5"] = pd.to_datetime(interventions5["T5"], format='%d%b%y:%H:%M:%S')
+
 df5 = pd.DataFrame({"CityName permanence": interventions5["Cityname Permanence"].str.upper(),
                     "CityName intervention": interventions5["Cityname Intervention"].str.upper(),
                     "Vector type": interventions5["Vector type NL"].str.upper(),
-                    "EventLevel Firstcall": interventions5["EventType and EventLevel"].str.upper()})
+                    "EventLevel Firstcall": interventions5["EventType and EventLevel"].str.upper(),
+                    "Time1": interventions5["T3"]-interventions5["T0"],
+                    "Time2": interventions5["T5"]-interventions5["T0"]})
 df5["CityName permanence"] = df5["CityName permanence"].str.extract(r'\((.*?)\)')
 df5["CityName permanence"] = df5["CityName permanence"].replace("BRUXELLES", "BRUSSEL")
 df5["CityName intervention"] = df5["CityName intervention"].str.extract(r'\((.*?)\)')
@@ -69,18 +102,46 @@ df5["CityName intervention"] = df5["CityName intervention"].replace("BRUXELLES",
 df5["Vector type"] = df5["Vector type"].replace("AMB", "AMBULANCE")
 df5["EventLevel Firstcall"] = df5["EventLevel Firstcall"].str.split(" ").str[1]
 df5["EventLevel Firstcall"] = df5["EventLevel Firstcall"].str.replace("0", "")
-#print(df5["EventLevel Firstcall"])
+#print(df5["Time2"])
 
 #cad
+cad["T0"] = pd.to_datetime(cad["T0"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+cad["T3"] = pd.to_datetime(cad["T3"], format='%Y-%m-%d %H:%M:%S.%f') # Convert the column back to datetime with the new format
+cad["T5"] = pd.to_datetime(cad["T5"], format='%Y-%m-%d %H:%M:%S.%f')
+
 df6 = pd.DataFrame({"CityName permanence": cad["Permanence long name"].str.upper(),
                     "CityName intervention": cad["CityName intervention"].str.upper(),
                     "Vector type": cad["Vector Type"].str.upper(),
-                    "EventLevel Firstcall": cad["EventLevel Trip"].str.upper()})
+                    "EventLevel Firstcall": cad["EventLevel Trip"].str.upper(),
+                    "Time1": cad["T3"]-cad["T0"],
+                    "Time2": cad["T5"]-cad["T0"]})
 df6["CityName permanence"] = df6["CityName permanence"].str.split(" ").str[1]
-#print(df6["EventLevel Firstcall"])
+df6["Time1"][df6["Time1"] < pd.Timedelta(0)] = pd.NaT
+df6["Time2"][df6["Time2"] < pd.Timedelta(0)] = pd.NaT
+#print(df6["Time2"])
 
 frames = [df1, df2, df3, df4, df5, df6]
 
-result = pd.concat(frames)
-print(result)
+data = pd.concat(frames)
+#print(data["CityName intervention"])
+
+##### DATA PREPROCESSING #####
+
+""" Vanaf hier data opsplitsen in train en test"""
+train, test = train_test_split(data, random_state=1)
+print(train)
+
+## Nagaan hoeveel data er ontbreekt
+city_permanence_count = data['CityName permanence'].isna().sum()
+#print(city_permanence_count/1045549) # 0.10775774258308314
+city_intervention_count = data['CityName intervention'].isna().sum()
+#print(city_intervention_count/1045549) # 0.005094930988408961
+vector_count = data['Vector type'].isna().sum()
+#print(vector_count/1045549) # 0.018477374087680253
+level_count = data['EventLevel Firstcall'].isna().sum()
+#print(level_count/1045549) # 0.026061906232993384
+time1_count = data['Time1'].isna().sum()
+#print(time1_count/1045549) # 0.2206171112018662
+time2_count = data['Time2'].isna().sum()
+#print(time2_count/1045549) # 0.3882247508246864
 
