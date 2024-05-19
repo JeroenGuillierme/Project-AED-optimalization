f53c00e5f3f6a55daea0d099617eacfc84b46ed8
ElenaPollet <168332247+ElenaPollet@users.noreply.github.com>
2024-05-19 16:33:26 +0200

    Update Response Time Analysis.py

--- Response Time Analysis.py
+++ Response Time Analysis.py
@@ -13,6 +13,7 @@ from datetime import datetime, timedelta
 
 
 """
+#Open data
 ambulance = pd.read_parquet('DATA/ambulance_locations.parquet.gzip')
 mug = pd.read_parquet('DATA/mug_locations.parquet.gzip')
 pit = pd.read_parquet('DATA/pit_locations.parquet.gzip')
@@ -27,7 +28,7 @@ aed = pd.read_parquet('DATA/aed_locations.parquet.gzip')
 pd.set_option('display.max_columns', None)
 
 
-##### DATA PRE-PREPROCESSING #####
+
 #Ambulance, mug, pit en aed bevatten geen responstijd variabelen dus gebruiken we niet
 
 
@@ -35,122 +36,139 @@ pd.set_option('display.max_columns', None)
 interventions1["T0"] = pd.to_datetime(interventions1["T0"], format='%d%b%y:%H:%M:%S')
 interventions1["T3"] = pd.to_datetime(interventions1["T3"], format='%Y-%m-%d %H:%M:%S.%f')
 interventions1["T5"] = pd.to_datetime(interventions1["T5"], format='%Y-%m-%d %H:%M:%S.%f')
-
 df1 = pd.DataFrame({
-    "CityName permanence": interventions1["CityName permanence"].str.upper(),
-    "CityName intervention": interventions1["CityName intervention"].str.upper(),
-    "Vector type": interventions1["Vector type"].str.upper(),
-    "EventLevel Firstcall": interventions1["EventLevel Firstcall"].str.upper(),
+    "Province": interventions1["Province intervention"].str.upper(),
+    "City Permanence": interventions1["CityName permanence"].str.upper(),
+    "City Intervention": interventions1["CityName intervention"].str.upper(),
+    "Vector": interventions1["Vector type"].str.upper(),
+    "Eventlevel": interventions1["EventLevel Firstcall"].str.upper(),
     "Time1": interventions1["T3"]-interventions1["T0"],
     "Time2": interventions1["T5"]-interventions1["T0"]})
-df1["CityName permanence"] = df1["CityName permanence"].str.extract(r'\((.*?)\)')
-df1["CityName intervention"] = df1["CityName intervention"].str.extract(r'\((.*?)\)')
+df1["Province"] = df1["Province"].replace("ANT","ANTWERPEN")
+df1["Province"] = df1["Province"].replace("OVL","OOST-VLAANDEREN")
+df1["Province"] = df1["Province"].replace("WVL","WEST-VLAANDEREN")
+df1["Province"] = df1["Province"].replace("HAI","HENEGOUWEN")
+df1["Province"] = df1["Province"].replace("BRW","WAALS_BRABANT")
+df1["Province"] = df1["Province"].replace("VBR","VLAAMS-BRABANT")
+df1["City Permanence"] = df1["City Permanence"].str.extract(r'\((.*?)\)')
+df1["City Intervention"] = df1["City Intervention"].str.extract(r'\((.*?)\)')
+df1["Eventlevel"] = df1["Eventlevel"].str.replace("A","")
+df1["Eventlevel"] = df1["Eventlevel"].str.replace("B","")
 df1["Time1"] = df1['Time1'].dt.total_seconds().round()
 df1["Time2"] = df1['Time2'].dt.total_seconds().round()
-print(df1["Time1"])
+
 
 # interventions2: CityName permanence, CityName intervention, Vector type, EventLevel Firstcall
 interventions2["T0"] = pd.to_datetime(interventions2["T0"], format='%d%b%y:%H:%M:%S')
 interventions2["T3"] = pd.to_datetime(interventions2["T3"], format='%Y-%m-%d %H:%M:%S.%f')
 interventions2["T5"] = pd.to_datetime(interventions2["T5"], format='%Y-%m-%d %H:%M:%S.%f')
-
-df2 = pd.DataFrame({"CityName permanence": interventions2["CityName permanence"].str.upper(),
-                    "CityName intervention": interventions2["CityName intervention"].str.upper(),
-                    "Vector type": interventions2["Vector type"].str.upper(),
-                    "EventLevel Firstcall": interventions2["EventLevel Firstcall"].str.upper(),
+df2 = pd.DataFrame({
+                    "Province": interventions2["Province intervention"].str.upper(),
+                    "City Permanence": interventions2["CityName permanence"].str.upper(),
+                    "City Intervention": interventions2["CityName intervention"].str.upper(),
+                    "Vector": interventions2["Vector type"].str.upper(),
+                    "Eventlevel": interventions2["EventLevel Firstcall"].str.upper(),
                     "Time1": interventions2["T3"]-interventions2["T0"],
                     "Time2": interventions2["T5"]-interventions2["T0"]})
-df2["CityName permanence"] = df2["CityName permanence"].str.extract(r'\((.*?)\)')
-df2["CityName intervention"] = df2["CityName intervention"].str.extract(r'\((.*?)\)')
+df2["Province"] = df2["Province"].replace("HAI","HENEGOUWEN")
+df2["Province"] = df2["Province"].replace("LIE","LUIK")
+df2["City Permanence"] = df2["City Permanence"].str.extract(r'\((.*?)\)')
+df2["City Intervention"] = df2["City Intervention"].str.extract(r'\((.*?)\)')
 df2["Time1"] = df2['Time1'].dt.total_seconds().round()
 df2["Time2"] = df2['Time2'].dt.total_seconds().round()
-#print(df2["Time2"])
 
 
 # interventions3: CityName permanence, CityName intervention, Vector type, EventLevel Firstcall
 interventions3["T0"] = pd.to_datetime(interventions3["T0"], format='%d%b%y:%H:%M:%S')
 interventions3["T3"] = pd.to_datetime(interventions3["T3"], format='%Y-%m-%d %H:%M:%S.%f')
 interventions3["T5"] = pd.to_datetime(interventions3["T5"], format='%Y-%m-%d %H:%M:%S.%f')
-
-df3 = pd.DataFrame({"CityName permanence": interventions3["CityName permanence"].str.upper(),
-                    "CityName intervention": interventions3["CityName intervention"].str.upper(),
-                    "Vector type": interventions3["Vector type"].str.upper(),
-                    "EventLevel Firstcall": interventions3["EventLevel Firstcall"].str.upper(),
+df3 = pd.DataFrame({
+                    "Province": interventions3["Province intervention"].str.upper(),
+                    "City Permanence": interventions3["CityName permanence"].str.upper(),
+                    "City Intervention": interventions3["CityName intervention"].str.upper(),
+                    "Vector": interventions3["Vector type"].str.upper(),
+                    "Eventlevel": interventions3["EventLevel Firstcall"].str.upper(),
                     "Time1": interventions3["T3"]-interventions3["T0"],
                     "Time2": interventions3["T5"]-interventions3["T0"]})
-df3["CityName permanence"] = df3["CityName permanence"].str.extract(r'\((.*?)\)')
-df3["CityName intervention"] = df3["CityName intervention"].str.extract(r'\((.*?)\)')
+df3["Province"] = df3["Province"].replace("LIE","LUIK")
+df3["Province"] = df3["Province"].replace("LIM","LIMBURG")
+df3["Province"] = df3["Province"].replace("NAM","NAMEN")
+df3["Province"] = df3["Province"].replace("LUX","LUXEMBURG")
+df3["City Permanence"] = df3["City Permanence"].str.extract(r'\((.*?)\)')
+df3["City Intervention"] = df3["City Intervention"].str.extract(r'\((.*?)\)')
 df3["Time1"] = df3['Time1'].dt.total_seconds().round()
 df3["Time2"] = df3['Time2'].dt.total_seconds().round()
-#print(df3["Time2"])
+
 
 # interventions4: cityname_permanence, CityName intervention, vector_type, eventLevel_firstcall
 interventions4["t0"] = pd.to_datetime(interventions4["t0"], format='%Y-%m-%d %H:%M:%S.%f %z')
 interventions4["t3"] = pd.to_datetime(interventions4["t3"], format='%Y-%m-%d %H:%M:%S.%f %z')
 interventions4["t5"] = pd.to_datetime(interventions4["t5"], format='%Y-%m-%d %H:%M:%S.%f %z')
-
-df4 = pd.DataFrame({"CityName permanence": interventions4["cityname_permanence"].str.upper(),
-                    "CityName intervention": interventions4["cityname_intervention"].str.upper(),
-                    "Vector type": interventions4["vector_type"].str.upper(),
-                    "EventLevel Firstcall": interventions4["eventLevel_firstcall"].str.upper(),
+df4 = pd.DataFrame({
+                    "Province": ["BRUSSEL"]*len(interventions4),
+                    "City Permanence": interventions4["cityname_permanence"].str.upper(),
+                    "City Intervention": interventions4["cityname_intervention"].str.upper(),
+                    "Vector": interventions4["vector_type"].str.upper(),
+                    "Eventlevel": interventions4["eventLevel_firstcall"].str.upper(),
                     "Time1": interventions4["t3"]-interventions4["t0"],
                     "Time2": interventions4["t5"]-interventions4["t0"]})
-df4["CityName permanence"] = df4["CityName permanence"].str.split(" \(").str[0]
-df4["CityName permanence"] = df4["CityName permanence"].replace("BRUXELLES", "BRUSSEL")
-df4["CityName intervention"] = df4["CityName intervention"].str.split(" \(").str[0]
-df4["CityName intervention"] = df4["CityName intervention"].replace("BRUXELLES", "BRUSSEL")
+df4["City Permanence"] = df4["City Permanence"].str.split(" \(").str[0]
+df4["City Permanence"] = df4["City Permanence"].replace("BRUXELLES", "BRUSSEL")
+df4["City Intervention"] = df4["City Intervention"].str.split(" \(").str[0]
+df4["City Intervention"] = df4["City Intervention"].replace("BRUXELLES", "BRUSSEL")
 df4["Time1"] = df4['Time1'].dt.total_seconds().round()
 df4["Time2"] = df4['Time2'].dt.total_seconds().round()
-#print(df4["Time2"])
 
 
 #interventions5
 interventions5["T0"] = pd.to_datetime(interventions5["T0"], format='%d%b%y:%H:%M:%S')
 interventions5["T3"] = pd.to_datetime(interventions5["T3"], format='%d%b%y:%H:%M:%S')
 interventions5["T5"] = pd.to_datetime(interventions5["T5"], format='%d%b%y:%H:%M:%S')
-
-df5 = pd.DataFrame({"CityName permanence": interventions5["Cityname Permanence"].str.upper(),
-                    "CityName intervention": interventions5["Cityname Intervention"].str.upper(),
-                    "Vector type": interventions5["Vector type NL"].str.upper(),
-                    "EventLevel Firstcall": interventions5["EventType and EventLevel"].str.upper(),
+df5 = pd.DataFrame({
+                    "Province": ["BRUSSEL"]*len(interventions5),
+                    "City Permanence": interventions5["Cityname Permanence"].str.upper(),
+                    "City Intervention": interventions5["Cityname Intervention"].str.upper(),
+                    "Vector": interventions5["Vector type NL"].str.upper(),
+                    "Eventlevel": interventions5["EventType and EventLevel"].str.upper(),
                     "Time1": interventions5["T3"]-interventions5["T0"],
                     "Time2": interventions5["T5"]-interventions5["T0"]})
-df5["CityName permanence"] = df5["CityName permanence"].str.extract(r'\((.*?)\)')
-df5["CityName permanence"] = df5["CityName permanence"].replace("BRUXELLES", "BRUSSEL")
-df5["CityName intervention"] = df5["CityName intervention"].str.extract(r'\((.*?)\)')
-df5["CityName intervention"] = df5["CityName intervention"].replace("BRUXELLES", "BRUSSEL")
-df5["Vector type"] = df5["Vector type"].replace("AMB", "AMBULANCE")
-df5["EventLevel Firstcall"] = df5["EventLevel Firstcall"].str.split(" ").str[1]
-df5["EventLevel Firstcall"] = df5["EventLevel Firstcall"].str.replace("0", "")
+df5["City Permanence"] = df5["City Permanence"].str.extract(r'\((.*?)\)')
+df5["City Permanence"] = df5["City Permanence"].replace("BRUXELLES", "BRUSSEL")
+df5["City Intervention"] = df5["City Intervention"].str.extract(r'\((.*?)\)')
+df5["City Intervention"] = df5["City Intervention"].replace("BRUXELLES", "BRUSSEL")
+df5["Vector"] = df5["Vector"].replace("AMB", "AMBULANCE")
+df5["Eventlevel"] = df5["Eventlevel"].str.split(" ").str[1]
+df5["Eventlevel"] = df5["Eventlevel"].str.replace("0", "")
 df5["Time1"] = df5['Time1'].dt.total_seconds().round()
 df5["Time2"] = df5['Time2'].dt.total_seconds().round()
-#print(df5["Time2"])
+
 
 #cad
 cad["T0"] = pd.to_datetime(cad["T0"], format='%Y-%m-%d %H:%M:%S.%f')
 cad["T3"] = pd.to_datetime(cad["T3"], format='%Y-%m-%d %H:%M:%S.%f')
 cad["T5"] = pd.to_datetime(cad["T5"], format='%Y-%m-%d %H:%M:%S.%f')
-
-df6 = pd.DataFrame({"CityName permanence": cad["Permanence long name"].str.upper(),
-                    "CityName intervention": cad["CityName intervention"].str.upper(),
-                    "Vector type": cad["Vector Type"].str.upper(),
-                    "EventLevel Firstcall": cad["EventLevel Trip"].str.upper(),
+df6 = pd.DataFrame({
+                    "Province": cad["province"].str.upper(),
+                    "City Permanence": cad["Permanence long name"].str.upper(),
+                    "City Intervention": cad["CityName intervention"].str.upper(),
+                    "Vector": cad["Vector Type"].str.upper(),
+                    "Eventlevel": cad["EventLevel Trip"].str.upper(),
                     "Time1": cad["T3"]-cad["T0"],
                     "Time2": cad["T5"]-cad["T0"]})
-df6["CityName permanence"] = df6["CityName permanence"].str.split(" ").str[1]
-df6["Time1"][df6["Time1"] < pd.Timedelta(0)] = pd.NaT
-df6["Time2"][df6["Time2"] < pd.Timedelta(0)] = pd.NaT
+df6["Province"] = df6["Province"].replace("NAM","NAMEN")
+df6["Province"] = df6["Province"].replace("WVL","WEST-VLAANDEREN")
+df6["Province"] = df6["Province"].replace("OVL","OOST-VLAANDEREN")
+df6["Province"] = df6["Province"].replace("VBR","VLAAMS-BRABANT")
+df6["City Permanence"] = df6["City Permanence"].str.split(" ").str[1]
 df6["Time1"] = df6['Time1'].dt.total_seconds().round()
 df6["Time2"] = df6['Time2'].dt.total_seconds().round()
-#print(df6["Time2"])
+df6["Time1"][df6['Time1'] < 0] = pd.NaT
+df6["Time2"][df6['Time2'] < 0] = pd.NaT
+
 
 frames = [df1, df2, df3, df4, df5, df6]
-
 data = pd.concat(frames)
-
 data.to_csv('DATA/interventions.csv', index=False)
-#print(data["CityName intervention"])
-
 """
 
 
